f205e1ba76be1f900b1750e4cb87fce8a85615e4
Raghul Ajith N <raghulajithn@gmail.com>
2024-02-25 17:18:10 +0530

    3 commit

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 let BASE_URL = "https://post-it-heroku.herokuapp.com/";
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-  BASE_URL = "http://localhost:4000/";
+  BASE_URL = "https://social-media-app-2-0vp6.onrender.com/";
 }
 
 export { BASE_URL };
